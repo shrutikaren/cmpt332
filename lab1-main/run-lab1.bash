ceb92a30564af8f48be7615fb8d805ff2b126669
Shruti <74854887+shrutikaren@users.noreply.github.com>
2024-09-12 23:45:23 -0600

    working on the bash file. figuring out how many command line arguments needed per binary file

---
+++ lab1-main/run-lab1.bash
@@ -0,0 +1,11 @@
+#Name: 
+#Student Number:
+#NSID: 
+
+#Approach: 
+#	Figure out number of command line arguments needed for each architecture
+# 	If a number is received, run that binary file
+#	Pass that command line agruments to the executable
+
+#!/bin/sh
+expected=$1
