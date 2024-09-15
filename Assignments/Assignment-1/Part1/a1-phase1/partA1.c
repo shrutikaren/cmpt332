3e7c849136a484b969114b18bb928a7d62a066a8
Shruti Kaur <ich524@usask.ca>
2024-09-15 13:29:20 -0600

    finished first draft of partA1

--- Assignments/Assignment-1/Part1/a1-phase1/partA1.c
+++ Assignments/Assignment-1/Part1/a1-phase1/partA1.c
@@ -4,8 +4,58 @@
 #include <windows.h>
 #include <time.h> 
 
-//create the thread structure 
+//thread structure to store thread information  
 typedef struct{
 	int idnum;
-	
+	int numSquare;	
 }Thread_Info;
+
+DWORD WINAPI ThreadFunction(LPVOID t){
+	(Thread_Info *) thread_data = (Thread_Info *) t;
+	int idnum = thread_data->idnum;
+	int numSquare = thread_data->numSquare;
+
+	clock_t start_time = clock(); 
+	
+	int counter = 0; 
+
+	for (int i = 0; (i <= numSquare) && keepRunning; i++){
+		Square (i); //perform the square function
+		counter ++;
+	}
+	
+	clock_t end_time = clock();
+	clock_t time = (start_time - end_time) / CLOCKS_PER_SECOND;
+	
+	return 0;
+
+}
+
+int main(int argc, char * argv[]){
+	int num_of_threads = atoi(argv[1]);
+	int deadlines = atoi(argv[2]);
+	int size = atoi(argv[3]);
+
+	HANDLE * threads = (HANDLE *)malloc(num_of_threads * sizeof(HANDLE));
+	Thread_Info * thread_data = (Thread_Info *)malloc 
+					(num_of_threads * sizeof(ThreadData));
+
+	//creating the threads
+	for (int i = 0; i < num_of_threads; i++){
+		thread_data[i].idnum = i;
+		thread_data[i].numSquare = size;
+		threads[i] = CreateThread(NULL, 0, ThreadFunction, 
+				&thread_data[i], 0, NULL);
+	}
+
+	Sleep(deadlines*1000); 
+
+	keepRunning = 0;
+
+	WaitForMultipleObjects(num_of_threads, threads,true, INFINITE);
+
+	free(threads);
+	free(malloc);
+
+	return 0;
+}
