8a6899f8f2cc226ac9718d7c43d68f32f1519293
Shruti Kaur <ich524@usask.ca>
2024-09-15 12:19:50 -0600

    finished square.c

--- Assignments/Assignment-1/Part1/a1-phase1/square.c
+++ Assignments/Assignment-1/Part1/a1-phase1/square.c
@@ -4,6 +4,15 @@
 volatile int keepRunning = 1; //should it be volatile tho? 
 // unsure because the question says "without synchronization"
 
-int square(int N){
 
+/*
+ * Purpose: Calculate the square of the number provided in the function
+ */
+int square(int N){
+	if (N == 0){
+ 		return 0;
+	}
+	else{
+		square(N-1) + N + N -1; 
+ 	}
 }
