42b4e5173a0b01b5a755b8ded738ed4b16d3e0b3
Antonio Turley <87983870+AntonioTurley@users.noreply.github.com>
2024-10-02 22:49:36 -0400

    Update partA3.c

--- Assignments/Assignment-3/a1-phase3/partA3.c
+++ Assignments/Assignment-3/a1-phase3/partA3.c
@@ -1,157 +1,149 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <square.h>
+#include "square.h"
 #include <pthread.h>
-#include <signal.h>
 #include <sys/time.h>
 #include <unistd.h>
 #include <stdbool.h>
 
 /* Thread structure to store thread information */
 typedef struct {
-	int idnum;
-	int numSquare;
-	struct timeval startTime, endTime; /*in-built structure*/
-	bool finished;	
-	int count;
+    int idnum;
+    int numSquare;
+    struct timeval startTime, endTime;
+    volatile bool finished;    
 } Thread_Info;
 
+/* Global variables required by square.c */
+volatile bool keepRunning = true;
+volatile int *squareCounts = NULL;
+volatile thread_id_t *thread_ids = NULL;
+int num_of_threads = 0;
+
 /*
- * Purpose: To help us work with the threads to go into the square functions
+ * Thread function that performs square calculations.
  */
+void* ThreadFunction(void* param) {
+    Thread_Info *thread_info = (Thread_Info *)param;
+    int i;
 
-volatile int threads_done = 0; 
-pthread_mutex_t thread_lock;
+    /* Store current thread ID */
+    thread_id_t current_thread_id = pthread_self();
 
-void* ThreadFunction(void* param){
-	Thread_Info *thread_id = (Thread_Info*) param;
-	int i;
+    /* Save thread ID in global array */
+    int threadIndex = thread_info->idnum - 1;
+    thread_ids[threadIndex] = current_thread_id;
 
-	gettimeofday(&thread_id->startTime, NULL);
+    /* Start timing */
+    gettimeofday(&thread_info->startTime, NULL);
 
-	for (i=1; i<= thread_id->numSquare; i++){
-		square(i);
-		thread_id->count++;
-		printf("This is iteration number %d\n", thread_id->count);
-	}
+    /* Perform calculations */
+    for (i = 0; i < thread_info->numSquare && keepRunning; i++) {
+        square(i);
+        printf("Thread %d: Iteration %d\n", thread_info->idnum, i + 1);
+    }
 
-	gettimeofday(&thread_id->endTime, NULL);
-	thread_id->finished = true;
-	
-	/*working with mutexes to prevent threads_done to be affected*/
-	pthread_mutex_lock(&thread_lock);
-	threads_done = threads_done + 1;
-	pthread_mutex_unlock(&thread_lock);
+    /* End timing */
+    gettimeofday(&thread_info->endTime, NULL);
+    thread_info->finished = true;
 
-	printf("Got to procedure ThreadFunc for the thread %d\n", 
-		thread_id->idnum);
-	return NULL;
+    printf("Got to procedure ThreadFunction for thread %d\n", thread_info->idnum);
+
+    return NULL; /* Exit the thread */
 }
 
-/*
- * Purpose: Create the threads and ensure the agruments passed are valid
- */
-int main(int argc, char * argv[]) {
-	
-	pthread_t * threads;
-	int num_of_threads, deadline, size, i;
-	Thread_Info * thread_data;
-	long elapsed_time;
 
-	if (argc != 4) {
-		printf("Error in procedure main: Invalid number of"
-			" parameters\n");
-		return -1;
-	}
+int main(int argc, char *argv[]) {
+    pthread_t *threads;
+    int deadline, size, i;
+    Thread_Info *thread_data;
+    long elapsed_time;
 
-	/* Parsing the arguments */
-	
-	num_of_threads = atoi(argv[1]);
-	deadline = atoi(argv[2]);
-	size = atoi(argv[3]);
+    if (argc != 4) {
+        printf("Error in main: Invalid number of parameters\n");
+        return -1;
+    }
 
-	if (num_of_threads <= 0){
-		printf("Error in procedure main: Invalid # of threads\n");
-		return -1;
- 	}
+    /* Parsing the arguments */
+    num_of_threads = atoi(argv[1]);
+    deadline = atoi(argv[2]);
+    size = atoi(argv[3]);
 
-	if (deadline <= 0){
-		printf("Error in procedure main: Invalid # of deadline\n");
-		return -1;
-	}
+    if (num_of_threads <= 0 || deadline <= 0 || size <= 0) {
+        printf("Error in main: Invalid arguments\n");
+        return -1;
+    }
 
-	if (size <= 0){
-		printf("Error in procedure main: Invalid # of size\n");
-		return -1;
-	}
-	
-	threads = (pthread_t *)malloc(num_of_threads * sizeof(pthread_t));
-	thread_data = (Thread_Info *) malloc(
-			num_of_threads * sizeof(Thread_Info));
+    /* Allocate memory for threads and thread data */
+    threads = (pthread_t *)malloc(num_of_threads * sizeof(pthread_t));
+    thread_data = (Thread_Info *)malloc(num_of_threads * sizeof(Thread_Info));
+    squareCounts = (volatile int *)malloc(num_of_threads * sizeof(int));
+    thread_ids = (volatile thread_id_t *)malloc(num_of_threads * sizeof(thread_id_t));
 
-	if (!threads || !thread_data) {
-		printf("Error in procedure main: Main allocation failed\n");
-		return -1;
-	}
+    if (!threads || !thread_data || !squareCounts || !thread_ids) {
+        printf("Error in main: Memory allocation failed\n");
+        return -1;
+    }
 
-	/* creating the threads */
-	for (i = 0; i < num_of_threads; i++){
-		thread_data[i].idnum = i + 1;
-		thread_data[i].numSquare = size;
-		thread_data[i].finished = false;
-		if (pthread_create (&threads[i], NULL, ThreadFunction, 
-		&thread_data[i])!=0){
-			printf("Error in procedure CreateThread: Failed to" 
-			"create thread %d\n", i);
-			return -1;
-		}
-	}
+    /* Initialize thread data and create threads */
+    for (i = 0; i < num_of_threads; i++) {
+        thread_data[i].idnum = i + 1;
+        thread_data[i].numSquare = size;
+        thread_data[i].finished = false;
+        squareCounts[i] = 0;
+        thread_ids[i] = 0;
 
-	sleep(deadline); /*in linux, it is limited to seconds not milliseconds*/
-	
- 	for (i = 0; i < num_of_threads; i++){
-		if (!thread_data[i].finished){
-			/*only one argument*/
-			pthread_cancel(threads[i]);
-			gettimeofday(&thread_data[i].endTime, NULL);
+        /* Create thread */
+        if (pthread_create(&threads[i], NULL, ThreadFunction, &thread_data[i]) != 0) {
+            printf("Error in pthread_create: Failed to create thread %d\n", i);
+            return -1;
+        }
+    }
 
-			elapsed_time = ((thread_data[i].endTime.tv_sec - 
-				thread_data[i].startTime.tv_sec) * 1000) + 
-				((thread_data[i].endTime.tv_usec - 
-				thread_data[i].startTime.tv_usec)/ 1000);
-		
-			printf("Thread %d finished at the elapsed time" 
-			" milliseconds of %ld\n",thread_data[i].idnum, 
-			elapsed_time);	
-		}
-		else{ 
-			/*if the threads were finished*/
-			gettimeofday(&thread_data[i].endTime, NULL);
+    sleep(deadline); /* Wait for the deadline */
 
-			elapsed_time = ((thread_data[i].endTime.tv_sec - 
-				thread_data[i].startTime.tv_sec) * 1000) +
-				((thread_data[i].endTime.tv_usec - 
-				thread_data[i].startTime.tv_usec)/ 1000);
-		
-			printf("Thread %d finished at the elapsed time" 
-			" milliseconds of %ld\n",thread_data[i].idnum, 
-			elapsed_time);	
-		}
-	}
-	
-	/*while loop continues indefinitely until condition is met
-  	condition: threads_done == num_of_threads - helps us to wait
-	for all threads to finish*/
-	while (true){
-		pthread_mutex_lock(&thread_lock);
-		if (threads_done == num_of_threads){
-			pthread_mutex_unlock(&thread_lock); /*avoid deadlock*/
-			break;
-		}
-		pthread_mutex_unlock(&thread_lock);
-	}
-	free(threads);
-	free(thread_data);
+    /* Terminate threads that haven't finished */
+    keepRunning = false;
 
-	return 0;
+    for (i = 0; i < num_of_threads; i++) {
+        if (!thread_data[i].finished) {
+            pthread_cancel(threads[i]);
+            gettimeofday(&thread_data[i].endTime, NULL);
+
+            elapsed_time = ((thread_data[i].endTime.tv_sec - 
+		    		thread_data[i].startTime.tv_sec) * 1000) +
+                           ((thread_data[i].endTime.tv_usec - t
+		    		hread_data[i].startTime.tv_usec) / 1000);
+
+            printf("Thread %d terminated after %ld milliseconds\n", 
+		    thread_data[i].idnum, elapsed_time);
+        } else {
+            elapsed_time = ((thread_data[i].endTime.tv_sec - 
+		    		thread_data[i].startTime.tv_sec) * 1000) +
+                           ((thread_data[i].endTime.tv_usec - 
+		    		thread_data[i].startTime.tv_usec) / 1000);
+
+            printf("Thread %d completed in %ld milliseconds\n", 
+		    thread_data[i].idnum, elapsed_time);
+        }
+    }
+
+    /* Join threads */
+    for (i = 0; i < num_of_threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    /* Print the number of times square() was called by each thread */
+    for (i = 0; i < num_of_threads; i++) {
+        printf("Thread %d: square() was called %d times\n", 
+		thread_data[i].idnum, squareCounts[i]);
+    }
+
+    /* Free allocated memory */
+    free(threads);
+    free(thread_data);
+    free((void *)squareCounts);
+    free((void *)thread_ids);
+
+    return 0;
 }
