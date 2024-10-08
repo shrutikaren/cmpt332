43a2dd92da4637566f1b64fd19f25f12b6527356
fldev <devteam@fldev.orl>
2024-10-07 21:52:37 -0400

    Update Repo

--- .ccls-cache/@home@devteam@students@cmpt332/Assignments@Assignment-4@Monitor.h
+++ .ccls-cache/@home@devteam@students@cmpt332/Assignments@Assignment-4@Monitor.h
@@ -10,8 +10,33 @@
 #ifndef MONITOR_H 
 #define MONITOR_H
 
+/* Default C-Library */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* UBC Threads */
 #include <os.h>
 #include <standards.h>
+
+/* Our List functions*/
+#include <list.h>
+
+/* Define the number of condition variables (k) */
+#define k 10
+
+typedef struct ConditionVariable{
+    LIST* queue;
+    int sem;
+} ConditionVariable;
+
+typedef struct Monitor{
+    LIST* enterQueue;
+    int mutex;
+    int enterSem;
+    ConditionVariable conVals[k];
+} Monitor;
+
+
 
 void MonEnter();
 void MonLeave();
