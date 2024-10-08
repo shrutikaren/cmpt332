d2c20b186e2f20ea94956a6509044b2c578262ca
fldev <devteam@fldev.orl>
2024-10-07 22:26:50 -0400

    Update monitor.h and .c

--- .ccls-cache/@home@devteam@students@cmpt332/Assignments@Assignment-4@Monitor.c
+++ .ccls-cache/@home@devteam@students@cmpt332/Assignments@Assignment-4@Monitor.c
@@ -4,6 +4,7 @@
  * 11339265
  */
 
+#include "list.h"
 #include <Monitor.h>
 
 static Monitor mon;
@@ -11,38 +12,49 @@ static Monitor mon;
 /* CMPT 332 GROUP 34 Change, Fall 2024 */
 /* Phase 1 */
 
-// Condition Variables
-/* 
- *  Objective: 
- *  ----------
- *
- *  Implement a monitor facility in C using only
- *  semaphore operations P() and V(). 
- *
- *  Key Points:
- *  -----------
- *
- *  Mutual Exclusion: Only one thread executes within the monitor at any given
- *  time.
- *  
- *  Condition Variables: Synchronize between threads using MonWait() and 
- *  MonSignal.
- *
- */ 
 
+/**
+ *  Initialize the monitor.
+ */ 
 void MonInit(){
 
+    int i;
+
     /* Initialize the mutex semaphore to 1 (unlock) */ 
-    mon.mutex = NewSem(1);
-    if(mon.mutex < 0){
-        printf(stderr, "Unable to create mutex semaphore");
+    mon.lock = NewSem(1);
+    if(mon.lock < 0){
+        perror("Log [Error] - Failed to create lock mutex in MonInit.");
         exit(EXIT_FAILURE);
     }
 
     /* Initialize the enter queue and its semaphore */  
-    mon.enterQueue = ListCreate();
-    if(mon.enterQueue == NULL){
+    mon.entryList = ListCreate();
+    if(mon.entryList == NULL){
+        perror("Log [Error] - Failed to create entry queue in MonInit.");
+        exit(EXIT_FAILURE);
+    }
 
+    /* Initialize the enterSem. */
+    mon.entrySem = NewSem(1);
+    if(mon.entrySem < 0){
+        perror("Log [Error] - Failed to create entry semaphore in MonInit.");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Initialize all conditional variables */
+    for(i = 0; i < k; i++){
+
+        mon.conVars[i].waitList = ListCreate();
+        if(mon.conVars[i].waitList == NULL){
+            fprintf(stderr, "%s\n");
+            exit(EXIT_FAILURE);
+        }
+
+        mon.conVars[i].semaphore = NewSem(0);
+        if(mon.conVars[i].waitList == NULL){
+            fprintf(stderr, "%s\n");
+            exit(EXIT_FAILURE);
+        }
 
     }
 
@@ -58,6 +70,10 @@ void MonLeave(){
 
 void MonWait(int var){
 
+    if(var < 0 || var >= k){
+        exit(EXIT_FAILURE);
+    }
+
 }
 
 void MonSignal(int var){
@@ -67,5 +83,12 @@ void MonSignal(int var){
     }
 
     /* Check if there are threads waiting on the condition varaible. */
+    if(ListCount(mon.conVars[var].waitList) > 0){
+        /* Remove the first thread from the condition variables queue. */ 
+        if ( == NULL){
+            exit(EXIT_FAILURE);
+        }
 
+        /* Signal the cv's semaphore to wake up the waiting thread */
+    }
 }
