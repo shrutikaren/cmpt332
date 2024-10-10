e8fc07192282b25da8dcc23710e02148972796a3
Shruti Kaur <ich524@usask.ca>
2024-10-09 19:15:28 -0600

    finished the RttMonInit() function

--- Labs/lab4-main/monitor.c
+++ Labs/lab4-main/monitor.c
@@ -14,11 +14,18 @@ ConditionVariables conV[k];
 
 void RttMonInit(int numI){
 	monitor.enterq = ListCreate(); /*Create a list for the enter queue*/
+	monitor.urgentq = ListCreate(); 
+	monitor.lock = -1 ; 
 	
+	for (int i = 0; i < = numI; i ++){
+		condV[i].waitlist = ListCreate();
+	}		
 	return NULL;
 }
 
 void RttMonEnter(){
+	/* If the monitor is not busy */
+	
 	return NULL;
 }
 
