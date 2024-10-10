f05bc99660efea116e53781652148e9743f94391
Shruti Kaur <ich524@usask.ca>
2024-10-09 18:26:18 -0600

    adding functions in the c file and working my way through it

--- Labs/lab4-main/monitor.c
+++ Labs/lab4-main/monitor.c
@@ -3,3 +3,31 @@
 	ICH524
 	11339265
 */
+
+#include "monitor.h"
+#include "rtthreads.h"
+
+#define k 5 /*picked an arbitarily number*/
+
+Monitor monitor;
+ConditionVariables conV[k];
+
+void RttMonInit(int numI){
+	
+}
+
+void RttMonEnter(){
+
+}
+
+void RttMonLeave(){
+
+}
+
+void RttMonSignal(int numS){
+
+}
+
+void MonServer(){
+
+}
