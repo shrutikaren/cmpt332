4cd19edd82bd33f20703654445773083e8ec63c7
Shruti Kaur <ich524@usask.ca>
2024-10-15 18:52:46 -0600

    added the header file

--- Labs/lab4-main-p2/rtt/reader-writer-monitor.h
+++ Labs/lab4-main-p2/rtt/reader-writer-monitor.h
@@ -1,11 +1,10 @@
-/* reader-writer.h
- * Dwight Makaroff October 27, 2011
- * necessary because I split up the 2 source files
- * for the reader-writer problem
- */
+#ifdef READER_WRITER_MONITOR_H
+#define READER_WRITER_MONITOR_H
 
 void Initialize(void);
-void StartRead(void);
-void StopRead(void);
-void StartWrite(void);
-void StopWrite(void);
+void StartRead();
+void StopRead();
+void StartWrite();
+void StopWrite();
+
+#endif /* READER_WRITER_MONITOR_H */
