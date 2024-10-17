9334c7fd4978bdaf29be78313c24faefa58b970e
fldev <devteam@fldev.orl>
2024-10-16 23:29:58 -0400

    Update Repo

--- Playground/list.h
+++ Playground/list.h
@@ -5,12 +5,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MIN_LISTS 10
+#define MIN_NODES 100
+
 /* Logging Errors in our code. */
 #define LOG_ERROR(msg) do {                             \
     fprintf(stderr, "Log [Error] - %s: %s at %s:%d\n",  \
         msg, strerror(errno), __FILE__, __LINE__);      \
     exit(EXIT_FAILURE);                                 \
 }while(0)
+
+/* Forward declarations */
+typedef struct NODE NODE;
+typedef struct LIST LIST;
+
+/* Function Prototypes */
+
+
 
 
 
