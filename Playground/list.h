4f3e0d61e5d04ec1c761774dc04d4c0833b0e6b2
fldev <devteam@fldev.orl>
2024-10-16 23:53:14 -0400

    Update Repo

--- Playground/list.h
+++ Playground/list.h
@@ -19,6 +19,49 @@
 typedef struct NODE NODE;
 typedef struct LIST LIST;
 
+/* NODE structure */
+struct NODE {
+    void *item;
+    int next;
+    int prev;
+};
+
+/* LIST structure */
+struct LIST {
+    int head;
+    int tail;
+    int current;
+    int count;
+    int inUse;
+};
+
+/* Node Pool structure */
+typedef struct {
+    NODE *nodes;
+    int *freeNodes;
+    int totalNodes;
+    int freeNodeCount;
+} NodePool;
+
+/* List Pool structure */
+typedef struct {
+    LIST *lists;
+    int *freeLists;
+    int totalLists;
+    int freeListCount;
+} ListPool;
+
+/* Global pools */
+static NodePool nodePool = {NULL, NULL, 0, 0};
+static ListPool listPool = {NULL, NULL, 0, 0};
+
+/* Helper Function prototypes */
+static void initializePools(void);
+static int allocateNode(void);
+static void freeNode(int index);
+static int allocateList(void);
+static void freeList(int index);
+
 /* Function Prototypes */
 
 /* List creation and management */
--- Playground/list_adders.c
+++ Playground/list_adders.c
@@ -4,51 +4,6 @@
 
 #define UNUSED_NODE -1
 
-/* NODE structure */
-struct NODE {
-    void *item;
-    int next;
-    int prev;
-};
-
-/* LIST structure */
-struct LIST {
-    int head;
-    int tail;
-    int current;
-    int count;
-    int inUse;
-};
-
-/* Node Pool structure */
-typedef struct {
-    NODE *nodes;
-    int *freeNodes;
-    int totalNodes;
-    int freeNodeCount;
-} NodePool;
-
-
-/* List Pool structure */
-typedef struct {
-    LIST *lists;
-    int *freeLists;
-    int totalLists;
-    int freeListCount;
-} ListPool;
-
-/* Global pools */
-static NodePool nodePool = {NULL, NULL, 0, 0};
-static ListPool listPool = {NULL, NULL, 0, 0};
-
-/* Function prototypes */
-static void initializePools(void);
-static int allocateNode(void);
-static void freeNode(int index);
-static int allocateList(void);
-static void freeList(int index);
-
-
 static void initializePools(void){
 
 }
