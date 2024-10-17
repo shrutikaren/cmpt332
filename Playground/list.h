d7b059ea24ca4a24b3f4cdf0be03cb3fe4fa4851
Antonio Turley <87983870+AntonioTurley@users.noreply.github.com>
2024-10-16 23:34:41 -0400

    Update list.h

--- Playground/list.h
+++ Playground/list.h
@@ -21,8 +21,31 @@ typedef struct LIST LIST;
 
 /* Function Prototypes */
 
+/* List creation and management */
+LIST *ListCreate(void);
+void ListFree(LIST *pList, void (*itemFree)(void *pItem));
 
+/* List properties */
+int ListCount(LIST *pList);
+void *ListFirst(LIST *pList);
+void *ListLast(LIST *pList);
+void *ListCurr(LIST *pList);
 
+/* List navigation */
+void *ListNext(LIST *pList);
+void *ListPrev(LIST *pList);
 
+/* List modification */
+int ListAdd(LIST *pList, void *pItem);
+int ListInsert(LIST *pList, void *pItem);
+int ListAppend(LIST *pList, void *pItem);
+int ListPrepend(LIST *pList, void *pItem);
+void *ListRemove(LIST *pList);
+void ListConcat(LIST *pList1, LIST *pList2);
+void *ListTrim(LIST *pList);
+
+/* List searching */
+void *ListSearch(LIST *pList,
+                 int (*comparator)(void *, void *), void *pComparisonArg);
 
 #endif 
