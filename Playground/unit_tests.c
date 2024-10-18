25a9f5c5b9b45f7c96cf79ae140573223861f6ac
Shruti Kaur <ich524@usask.ca>
2024-10-17 19:34:38 -0600

    adding more test cases

--- Playground/unit_tests.c
+++ Playground/unit_tests.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include "list.h"
 
+/* Checking if list was created */
 void test_ListCreate(){
     LIST* list = ListCreate();
     assert(list != NULL);
@@ -9,6 +10,7 @@ void test_ListCreate(){
     printf("test_ListCreate passed.\n");
 }
 
+/* Checking if list created with one item */
 void test_ListAdd(){
     LIST* list = ListCreate();
     int item1 = 3;
@@ -19,11 +21,18 @@ void test_ListAdd(){
     printf("test_ListAdd passed.\n");
 }
 
+void test2_ListAdd(){
+    LIST* list = NULL;
+    int item1 = 4;
+    int result = ListAdd(list, &item1);
+    assert(result==EXIT_FAILURE);
+    printf("test2_ListAdd passed.\n");
+}
 int main(int argc, char *argv[]){
     
     test_ListCreate();
     test_ListAdd();
-
+    test2_ListAdd();
     /* Remove all the memory at the end. */
     ListDispose();
 
