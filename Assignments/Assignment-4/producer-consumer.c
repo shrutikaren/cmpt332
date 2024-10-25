548dcaf0ac3e3a5ded182a5d81a2e6f2492f59c4
Shruti Kaur <ich524@usask.ca>
2024-10-25 14:21:43 -0600

    had some time so stared working on it for a bit - will come back to this afer my lab

--- Assignments/Assignment-4/producer-consumer.c
+++ Assignments/Assignment-4/producer-consumer.c
@@ -1,1 +1,17 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+
+#define BUFFER_SIZE 10
+
+/* Implementing a bounded buffer and semaphore*/
+typedef struct thread_info_t{
+	LIST* buffer;
+	int sempahore;
+}
+
+thread_info_t thread_info = malloc(sizeof(BUFFER_SIZE));
+
 
