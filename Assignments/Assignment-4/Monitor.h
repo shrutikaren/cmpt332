264240525837e075624db55b649b40a2344568ea
Antonio Turley <87983870+AntonioTurley@users.noreply.github.com>
2024-10-28 00:28:23 -0400

    Update Monitor.h

--- Assignments/Assignment-4/Monitor.h
+++ Assignments/Assignment-4/Monitor.h
@@ -1,4 +1,4 @@
-#ifndef MONITOR_H 
+ifndef MONITOR_H 
 #define MONITOR_H
 
 /* Default C-Library */
@@ -8,34 +8,6 @@
 #include <string.h>
 #define NO_CVS 7
 
-<<<<<<< HEAD
-/* UBC Threads */
-#include <os.h>
-
-/* Our List functions */
-#include <list.h>
-
-/* Define the number of condition variables (k) */
-#define k 10
-
-typedef struct CV {
-    int sem;           /* semaphore for condition variable */
-    LIST* wait_queue;  /* list of threads waiting */
-} CV;
-
-typedef struct Monitor {
-    CV cond_vars[k];          /* array of condition variables */
-    int enter_mtx;            /* semaphore for entering monitor */
-    int urgent_sem;           /* semaphore for urgent threads */
-    int enter_queue_sem;      /* semaphore for accessing enter_queue */
-    LIST* urgent_queue;       /* prioritized queue */
-    LIST* enter_queue;        /* queue for monitor entry */
-} Monitor;
-
-/* Monitor Function Declarations */
-void MonInit();
-=======
->>>>>>> refs/remotes/origin/main
 void MonEnter(void);
 void MonLeave(void);
 void MonWait(int);
