6bf5006232163d5888a3e5df75073b4fd205d287
Antonio Turley <87983870+AntonioTurley@users.noreply.github.com>
2024-10-27 23:49:34 -0400

    Update Monitor.h

--- Assignments/Assignment-4/Monitor.h
+++ Assignments/Assignment-4/Monitor.h
@@ -21,26 +21,23 @@
 /* Our List functions */
 #include <list.h>
 
-/* Loggin Errors in our code. */
-
+/* Logging Errors in our code. */
 #define LOG_ERROR(msg) do {                             \
     fprintf(stderr, "Log [Error] - %s: %s at %s:%d\n",  \
         msg, strerror(errno), __FILE__, __LINE__);      \
     exit(EXIT_FAILURE);                                 \
-}while(0)
+} while(0)
 
 /* Define the number of condition variables (k) */
 #define k 10
 
-typedef struct CondVar{
-    int semaphore; /* int to control access */
+typedef struct CondVar {
+    int semaphore; /* semaphore for condition variable */
     LIST* waitList; /* list of threads waiting */
 } CondVar;
 
-typedef struct Monitor{
-    int lock; /* used to access shared resources */
-    int entrySem; /* used to access entry */
-    LIST* entryList; /* threads to enter into the list */
+typedef struct Monitor {
+    int lock; /* semaphore used as the monitor lock */
     CondVar conVars[k]; 
 } Monitor;
 
