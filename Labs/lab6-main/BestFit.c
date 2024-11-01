5c2b091bbbede22ef13fe6298d9e91c5a938f16f
Shruti Kaur <ich524@usask.ca>
2024-11-01 07:42:52 -0600

    adding the thread_creation function for the BestFit.c

--- Labs/lab6-main/BestFit.c
+++ Labs/lab6-main/BestFit.c
@@ -9,7 +9,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <BestFitMonitor.h>
+#include <Monitor.h>
+
+void* thread_creation(void* arg){
+	int thread_id, seed, i;
+	thread_id = *(int*)arg;
+
+	/* Logic: Have time(NULL) to produce the seeconds. If I didn't use
+ 	   XOR as my operation, every thread would be having the same
+	   random number if all the threads were generated so closely with
+	   each other. */
+	srand(time(NULL) ^ (thread_id << 16);
+	
+	for (i = 0; i < numberOfIterations; i ++){
+		
+	}
+}
 
 int mainp(){
-
+	/* Initialize the memory space */
+	Initialize();
+		
+	
+	
 }
