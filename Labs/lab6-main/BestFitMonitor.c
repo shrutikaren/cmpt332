d4d8cbdc010dc0361f20ff5d3e05f5286ea15e3f
Shruti Kaur <ich524@usask.ca>
2024-10-31 21:09:15 -0600

    adding the latest changes in here

--- Labs/lab6-main/BestFitMonitor.c
+++ Labs/lab6-main/BestFitMonitor.c
@@ -3,3 +3,11 @@
  * NSID: ICH524
  * Student Number: 11339265
  * */
+
+#include <stdio.h>
+#include <Monitor.h>
+
+#define maxSleepTime
+#define maxAllocation
+#define freeProability
+#define numberOfIterations
