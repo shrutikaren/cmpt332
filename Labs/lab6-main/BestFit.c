e11a4fa19b10c69510ca0b36d95a262788e1ffd7
Shruti Kaur <ich524@usask.ca>
2024-11-01 07:23:14 -0600

    added a BestFitMonitor.h

--- Labs/lab6-main/BestFit.c
+++ Labs/lab6-main/BestFit.c
@@ -4,6 +4,11 @@
  * Student Number: 11339265
  * */
 
+#include <os.h>
+#include <standards.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <BestFitMonitor.h>
 
 int mainp(){
 
---
+++ Labs/lab6-main/BestFitMonitor.h
@@ -0,0 +1,32 @@
+/*
+ * Name: KAUR Shruti
+ * NSID: ICH524
+ * Student Number: 11339265
+ * */
+
+#ifndef BESTFITMONITOR_H
+#define BESTFITMONITOR_H
+
+#include <stdio.h>
+#include <Monitor.h>
+
+#define maxSleepTime 2
+#define maxAllocation 20480
+#define freeProability 0.3
+#define numberOfIterations 1000
+#define memsize 102400
+#define CondVariables 15
+#define Write 1
+#define Read 0
+
+typedef struct MemSpace{
+	int size;
+	int startAddress;
+	struct Memspace* next;
+}MemSpace;
+
+void Free(int startAddress, int size);
+void BF-Allocate(int size);
+void Initialize(void);
+
+#endif /* BESTFITMONIOR_H */
