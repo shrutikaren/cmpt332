26b7baaaf715c5476dd3cb9bf0aa488f61cc2b79
Shruti Kaur <ich524@usask.ca>
2024-10-31 23:49:02 -0600

    finished creating the mmory block daa strucure and the inialization function

--- Labs/lab6-main/BestFitMonitor.c
+++ Labs/lab6-main/BestFitMonitor.c
@@ -12,8 +12,46 @@
 #define freeProability 0.3
 #define numberOfIterations 1000
 #define memsize 102400
-void BF-Allocate(int size){
+#define CondVariables 15
+#define Write 1
+#define Read 0
+
+typedef struct MemSpace{
+	int size;
+	int startAddress;
+	struct Memspace* next;
+}MemSpace;
+
+
+/* Variabls defined ouside the function */
+static int totalMem = memsize;
+static MemSpace* list = NULL;
+
+/*
+ * Intialize(void) has a void parameter with a void 
+ * return type. This helps to iniialize our list with
+ * the data type of MemSpace.
+ * */
+void Initialize(void){
+	list = (MemSpace*)malloc(sizeof(MemSpace));
+
+	/* Case One: list is empty */
+	if (list == NULL){
+		printf("Failed to allocate memory to our memory list");
+		return;
+	}
 	
+	/* Case Two: list is not empty */
+	list->size = totalMem;
+	list->startAddress = 0;
+	list->next = NULL;
 }
 
-void Free(
+/*
+ * BF-Allocate is responsible for allocating memory. 
+ * 
+ * */
+void BF-Allocate(int size){
+}
+
+void Free(){}
