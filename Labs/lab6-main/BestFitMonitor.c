cbaf920505e207e8debc6aa004641bc1f6b364ac
Shruti Kaur <ich524@usask.ca>
2024-10-31 22:09:19 -0600

    adding bfallocate and free() code and design doc reasoning as well

--- Labs/lab6-main/BestFitMonitor.c
+++ Labs/lab6-main/BestFitMonitor.c
@@ -7,7 +7,13 @@
 #include <stdio.h>
 #include <Monitor.h>
 
-#define maxSleepTime
-#define maxAllocation
-#define freeProability
-#define numberOfIterations
+#define maxSleepTime 2
+#define maxAllocation 20480
+#define freeProability 0.3
+#define numberOfIterations 1000
+#define memsize 102400
+void BF-Allocate(int size){
+	
+}
+
+void Free(
