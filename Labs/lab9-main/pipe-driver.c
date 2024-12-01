c54d0c0d77ff014fcae054111df0a45b84f0c298
Antonio Turley <87983870+AntonioTurley@users.noreply.github.com>
2024-12-01 12:49:21 -0500

    Update pipe-driver.c

--- Labs/lab9-main/pipe-driver.c
+++ Labs/lab9-main/pipe-driver.c
@@ -1,4 +1,4 @@
-/* pipe_driver.c - Virtual FIFO Device Driver */
+/* pipe-driver.c - Virtual FIFO Device Driver */
 
 #include <linux/module.h>
 #include <linux/version.h>
@@ -12,12 +12,12 @@
 #include <linux/sched.h>
 #include <linux/init.h>
 
-#define DEVICE_NAME "fifo" // Device name
-#define N 1  // Number of FIFOs
+#define DEVICE_NAME "fifo" /* Device name */
+#define N 1  /* Number of FIFOs */
 
-#define BUFFER_SIZE 1024 // Size of the FIFO buffer
+#define BUFFER_SIZE 1024 /* Size of the FIFO buffer */
 
-// Structure to represent a FIFO buffer
+/* Structure to represent a FIFO buffer */
 struct fifo_buffer {
     char data[BUFFER_SIZE];
     int read_pos;
@@ -30,30 +30,34 @@ struct fifo_buffer {
     wait_queue_head_t write_queue;
 };
 
-static struct fifo_buffer fifos[N]; // Array of FIFO buffers
+static struct fifo_buffer fifos[N]; /* Array of FIFO buffers */
 
-static dev_t dev_num;      // Device number
-static struct cdev *my_cdev; // Character device structure
-static struct class *cls;    // Device class
+static dev_t dev_num;      /* Device number */
+static struct cdev *my_cdev; /* Character device structure */
+static struct class *cls;    /* Device class */
 
-// Open function
+/* Open function */
 static int device_open(struct inode *inode, struct file *file)
 {
-    int minor = iminor(inode);           // Get minor number
-    int fifo_index = minor / 2;          // Determine FIFO index
-    struct fifo_buffer *fifo = &fifos[fifo_index];
+    int minor;
+    int fifo_index;
+    struct fifo_buffer *fifo;
+
+    minor = iminor(inode);           /* Get minor number */
+    fifo_index = minor / 2;          /* Determine FIFO index */
+    fifo = &fifos[fifo_index];
 
     mutex_lock(&fifo->lock);
-    if (minor % 2 == 0) { // Write end
+    if (minor % 2 == 0) { /* Write end */
         if (fifo->is_open_write) {
             mutex_unlock(&fifo->lock);
-            return -EBUSY; // Device is busy
+            return -EBUSY; /* Device is busy */
         }
         fifo->is_open_write = 1;
-    } else { // Read end
+    } else { /* Read end */
         if (fifo->is_open_read) {
             mutex_unlock(&fifo->lock);
-            return -EBUSY; // Device is busy
+            return -EBUSY; /* Device is busy */
         }
         fifo->is_open_read = 1;
     }
@@ -62,59 +66,69 @@ static int device_open(struct inode *inode, struct file *file)
     return 0;
 }
 
-// Release function
+/* Release function */
 static int device_release(struct inode *inode, struct file *file)
 {
-    int minor = iminor(inode);           // Get minor number
-    int fifo_index = minor / 2;          // Determine FIFO index
-    struct fifo_buffer *fifo = &fifos[fifo_index];
+    int minor;
+    int fifo_index;
+    struct fifo_buffer *fifo;
+
+    minor = iminor(inode);           /* Get minor number */
+    fifo_index = minor / 2;          /* Determine FIFO index */
+    fifo = &fifos[fifo_index];
 
     mutex_lock(&fifo->lock);
-    if (minor % 2 == 0) { // Write end
+    if (minor % 2 == 0) { /* Write end */
         fifo->is_open_write = 0;
-        wake_up_interruptible(&fifo->read_queue); // Wake up readers
-    } else { // Read end
+        wake_up_interruptible(&fifo->read_queue); /* Wake up readers */
+    } else { /* Read end */
         fifo->is_open_read = 0;
-        wake_up_interruptible(&fifo->write_queue); // Wake up writers
+        wake_up_interruptible(&fifo->write_queue); /* Wake up writers */
     }
     mutex_unlock(&fifo->lock);
 
     return 0;
 }
 
-// Read function
+/* Read function */
 static ssize_t device_read(struct file *file, char __user *buf, size_t count, loff_t *offset)
 {
-    int minor = iminor(file_inode(file));  // Get minor number
-    int fifo_index = minor / 2;            // Determine FIFO index
-    struct fifo_buffer *fifo = &fifos[fifo_index];
+    int minor;
+    int fifo_index;
+    struct fifo_buffer *fifo;
     ssize_t ret = 0;
+    size_t bytes_to_read;
+    size_t first_chunk;
+
+    minor = iminor(file_inode(file));  /* Get minor number */
+    fifo_index = minor / 2;            /* Determine FIFO index */
+    fifo = &fifos[fifo_index];
 
     if (minor % 2 == 0) {
-        // Read operation not allowed on write end
+        /* Read operation not allowed on write end */
         return -EINVAL;
     }
 
     mutex_lock(&fifo->lock);
     while (fifo->count == 0) {
         if (!fifo->is_open_write) {
-            // Write end is closed, return EOF
+            /* Write end is closed, return EOF */
             ret = 0;
             goto out;
         }
         mutex_unlock(&fifo->lock);
-        // Wait for data or write end to close
+        /* Wait for data or write end to close */
         if (wait_event_interruptible(fifo->read_queue, fifo->count > 0 || !fifo->is_open_write)) {
-            return -ERESTARTSYS; // Interrupted by signal
+            return -ERESTARTSYS; /* Interrupted by signal */
         }
         mutex_lock(&fifo->lock);
     }
 
-    // Determine number of bytes to read
-    size_t bytes_to_read = min(count, (size_t)fifo->count);
-    size_t first_chunk = min(bytes_to_read, (size_t)(BUFFER_SIZE - fifo->read_pos));
+    /* Determine number of bytes to read */
+    bytes_to_read = min(count, (size_t)fifo->count);
+    first_chunk = min(bytes_to_read, (size_t)(BUFFER_SIZE - fifo->read_pos));
 
-    // Copy data to user space
+    /* Copy data to user space */
     if (copy_to_user(buf, fifo->data + fifo->read_pos, first_chunk)) {
         ret = -EFAULT;
         goto out;
@@ -123,7 +137,7 @@ static ssize_t device_read(struct file *file, char __user *buf, size_t count, lo
     fifo->count -= first_chunk;
     ret += first_chunk;
 
-    // Handle wrap-around
+    /* Handle wrap-around */
     if (bytes_to_read > first_chunk) {
         size_t second_chunk = bytes_to_read - first_chunk;
         if (copy_to_user(buf + first_chunk, fifo->data + fifo->read_pos, second_chunk)) {
@@ -135,47 +149,54 @@ static ssize_t device_read(struct file *file, char __user *buf, size_t count, lo
         ret += second_chunk;
     }
 
-    wake_up_interruptible(&fifo->write_queue); // Wake up writers
+    wake_up_interruptible(&fifo->write_queue); /* Wake up writers */
 
 out:
     mutex_unlock(&fifo->lock);
     return ret;
 }
 
-// Write function
+/* Write function */
 static ssize_t device_write(struct file *file, const char __user *buf, size_t count, loff_t *offset)
 {
-    int minor = iminor(file_inode(file)); // Get minor number
-    int fifo_index = minor / 2;           // Determine FIFO index
-    struct fifo_buffer *fifo = &fifos[fifo_index];
+    int minor;
+    int fifo_index;
+    struct fifo_buffer *fifo;
     ssize_t ret = 0;
+    size_t space_available;
+    size_t bytes_to_write;
+    size_t first_chunk;
+
+    minor = iminor(file_inode(file)); /* Get minor number */
+    fifo_index = minor / 2;           /* Determine FIFO index */
+    fifo = &fifos[fifo_index];
 
     if (minor % 2 != 0) {
-        // Write operation not allowed on read end
+        /* Write operation not allowed on read end */
         return -EINVAL;
     }
 
     mutex_lock(&fifo->lock);
     while (fifo->count == BUFFER_SIZE) {
         if (!fifo->is_open_read) {
-            // Read end is closed, cannot write more data
+            /* Read end is closed, cannot write more data */
             ret = -EPIPE;
             goto out;
         }
         mutex_unlock(&fifo->lock);
-        // Wait for space or read end to close
+        /* Wait for space or read end to close */
         if (wait_event_interruptible(fifo->write_queue, fifo->count < BUFFER_SIZE || !fifo->is_open_read)) {
-            return -ERESTARTSYS; // Interrupted by signal
+            return -ERESTARTSYS; /* Interrupted by signal */
         }
         mutex_lock(&fifo->lock);
     }
 
-    // Determine number of bytes to write
-    size_t space_available = BUFFER_SIZE - fifo->count;
-    size_t bytes_to_write = min(count, space_available);
-    size_t first_chunk = min(bytes_to_write, (size_t)(BUFFER_SIZE - fifo->write_pos));
+    /* Determine number of bytes to write */
+    space_available = BUFFER_SIZE - fifo->count;
+    bytes_to_write = min(count, space_available);
+    first_chunk = min(bytes_to_write, (size_t)(BUFFER_SIZE - fifo->write_pos));
 
-    // Copy data from user space
+    /* Copy data from user space */
     if (copy_from_user(fifo->data + fifo->write_pos, buf, first_chunk)) {
         ret = -EFAULT;
         goto out;
@@ -184,7 +205,7 @@ static ssize_t device_write(struct file *file, const char __user *buf, size_t co
     fifo->count += first_chunk;
     ret += first_chunk;
 
-    // Handle wrap-around
+    /* Handle wrap-around */
     if (bytes_to_write > first_chunk) {
         size_t second_chunk = bytes_to_write - first_chunk;
         if (copy_from_user(fifo->data + fifo->write_pos, buf + first_chunk, second_chunk)) {
@@ -196,14 +217,14 @@ static ssize_t device_write(struct file *file, const char __user *buf, size_t co
         ret += second_chunk;
     }
 
-    wake_up_interruptible(&fifo->read_queue); // Wake up readers
+    wake_up_interruptible(&fifo->read_queue); /* Wake up readers */
 
 out:
     mutex_unlock(&fifo->lock);
     return ret;
 }
 
-// File operations structure
+/* File operations structure */
 static struct file_operations fops = {
     .owner = THIS_MODULE,
     .open = device_open,
@@ -212,14 +233,14 @@ static struct file_operations fops = {
     .write = device_write,
 };
 
-// Module initialization function
+/* Module initialization function */
 static int __init fifo_init(void)
 {
     int ret;
     int major;
     int i;
 
-    // Allocate device numbers
+    /* Allocate device numbers */
     ret = alloc_chrdev_region(&dev_num, 0, 2 * N, DEVICE_NAME);
     if (ret < 0) {
         pr_alert("Failed to allocate char device region\n");
@@ -227,7 +248,7 @@ static int __init fifo_init(void)
     }
     major = MAJOR(dev_num);
 
-    // Create cdev structure
+    /* Create cdev structure */
     my_cdev = cdev_alloc();
     cdev_init(my_cdev, &fops);
     my_cdev->owner = THIS_MODULE;
@@ -238,15 +259,21 @@ static int __init fifo_init(void)
         return ret;
     }
 
-    // Create device class
+    /* Create device class */
     cls = class_create(THIS_MODULE, DEVICE_NAME);
+    if (IS_ERR(cls)) {
+        pr_alert("Failed to create device class\n");
+        cdev_del(my_cdev);
+        unregister_chrdev_region(dev_num, 2 * N);
+        return PTR_ERR(cls);
+    }
 
-    // Create device files
+    /* Create device files */
     for (i = 0; i < 2 * N; i++) {
         device_create(cls, NULL, MKDEV(major, i), NULL, "fifo%d", i);
     }
 
-    // Initialize FIFO buffers
+    /* Initialize FIFO buffers */
     for (i = 0; i < N; i++) {
         mutex_init(&fifos[i].lock);
         init_waitqueue_head(&fifos[i].read_queue);
@@ -262,24 +289,24 @@ static int __init fifo_init(void)
     return 0;
 }
 
-// Module exit function
+/* Module exit function */
 static void __exit fifo_exit(void)
 {
     int major = MAJOR(dev_num);
     int i;
 
-    // Destroy device files
+    /* Destroy device files */
     for (i = 0; i < 2 * N; i++) {
         device_destroy(cls, MKDEV(major, i));
     }
 
-    // Destroy device class
+    /* Destroy device class */
     class_destroy(cls);
 
-    // Delete cdev structure
+    /* Delete cdev structure */
     cdev_del(my_cdev);
 
-    // Unregister device numbers
+    /* Unregister device numbers */
     unregister_chrdev_region(dev_num, 2 * N);
 
     pr_info("FIFO driver exited\n");
@@ -289,5 +316,5 @@ module_init(fifo_init);
 module_exit(fifo_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("---------");
+MODULE_AUTHOR("Karen");
 MODULE_DESCRIPTION("Virtual FIFO Device Driver");
