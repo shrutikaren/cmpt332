aecbc738599423e4a7e78942d1a1adfd0c2fb164
Virtual-Karen <no@no.no>
2024-12-01 17:36:00 +0000

    working on the producer consumer part of the code

--- Labs/lab9-main/produceconsume.c
+++ Labs/lab9-main/produceconsume.c
@@ -21,11 +21,23 @@ static struct circular_buffer buffer = {
 	head = 0;
 	tail = 0;	
 };
+static spinlock_t lock_buffer;
+static wait_queue_head_t wait_queue;
 
 /* Creating the producer that produce those messages.
  * It will write data intothe circular buffer while
  * ensuring that it doesn't overwrite any data */
-
+ssize_t producer(const char *user_buffer, size_t count){
+	unsigned long flags;
+	size_t bytes_written = 0;
+	while (count > 0){
+		size_t space_remain = CIRC_SPACE(buffer.head, buffer.tail, BUF_SIZE);
+		if (space_remain == 0){
+			/* No more space remains - wait patiently */
+			spin_unlock_irqrestore(&lock_buffer, flags);
+		}
+	}
+}
 
 
 /* Consumer code that consumes the message produced.
