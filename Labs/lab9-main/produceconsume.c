476a60660daef45d80dfe41754c4ea240085f8aa
Virtual-Karen <no@no.no>
2024-12-01 17:20:23 +0000

    adding the prodconsum bit

--- Labs/lab9-main/produceconsume.c
+++ Labs/lab9-main/produceconsume.c
@@ -6,8 +6,29 @@
 #include <fcntl.h>
 #include <errno.h>
 
+/* More header files for this problem specifically */
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/spinlock.h>
+#include <linux/sched.h>
+#include <linux/wait.h>
+#include <linux/uaccess.h>
+
+#define BUF_SIZE 128
+static char buffer_data[BUF_SIZE];
+static struct circular_buffer buffer = {
+	buf = buffer_data;
+	head = 0;
+	tail = 0;	
+};
+
 /* Creating the producer that produce those messages.
  * It will write data intothe circular buffer while
  * ensuring that it doesn't overwrite any data */
 
-/* Consumer code that consumes the message produced */
+
+
+/* Consumer code that consumes the message produced.
+ * In simplest terms, you would read the data from 
+ * the circular buffer to ensure that there is no
+ * overhead data available. */
