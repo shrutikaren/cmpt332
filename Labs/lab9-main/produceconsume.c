e938a3ec125d0328a5976e58e9c6150e0d7d7d84
Virtual-Karen <no@no.no>
2024-12-01 17:02:33 +0000

    Adding my cd

--- Labs/lab9-main/produceconsume.c
+++ Labs/lab9-main/produceconsume.c
@@ -1,2 +1,13 @@
 #include <linux/circ_buf.h> /* For circular buffers */
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
 
+/* Creating the producer that produce those messages.
+ * It will write data intothe circular buffer while
+ * ensuring that it doesn't overwrite any data */
+
+/* Consumer code that consumes the message produced */
