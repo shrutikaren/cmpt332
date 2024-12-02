22abc108196e85d324678df7ceaa0c3dd99a08be
Shruti Kaur <ich524@usask.ca>
2024-12-02 16:07:07 -0600

    adding the initialization and exit parts of the code. including printk for printing in kernel as well

--- Labs/lab9-main/produceconsume.c
+++ Labs/lab9-main/produceconsume.c
@@ -31,10 +31,10 @@ ssize_t producer(const char *user_buffer, size_t count){
 	unsigned long flags;
 	size_t bytes_written = 0;
 	bool condition;
-	size_t remaining_space, space_to_end, min_to_write;
+	size_t remaining_space, min_to_write;
 	while (count > 0){
-		remaining_space = CIRC_SPACE(buffer.head, buffer.tail, BUF_SIZE);
-		if (remaining_space == 0){
+		size_t space_remain = CIRC_SPACE(buffer.head, buffer.tail, BUF_SIZE);
+		if (space_remain == 0){
 			/* No more space remains - wait patiently */
 			spin_unlock_irqrestore(&lock_buffer, flags);
 			if (CIRC_SPACEE(buffer.head, buffer.tail, BUF_SIZE) > 0){
@@ -47,20 +47,12 @@ ssize_t producer(const char *user_buffer, size_t count){
 			wait_event_interruptible(wait_queue, condition);
 			spin_lock_irqsave(&lock_buffer, flags);
 		}
-		space_to_end = CIRC_SPACE_TO_END(buffer.head, buffer.tail, BUF_SIZE);
+		remaining_space = CIRC_SPACE_TO_END(buffer.head, buffer.tail, BUF_SIZE);
 
-		min_to_write = min(remaining_space, count);
-		if (copy_from_user(buffer.buf + buffer.head, user_buffer + bytes_written, min_to_write)){
-			spin_unlock_irqrestore(&lock_buffer, flags);
-			return -EFAULT;
-		}
-		buffer.head = (buffer.head + min_to_write) & (BUF_SIZE - 1);
-		count -= min_to_write;
-		bytes_written += min_to_write;
+		size_t min_to_write = min(remaining_space, count);
+
 	}
-	spin_unlock_irqrestore(&lock_buffer, flags);
-	wake_up_interruptible(&wait_queue);
-	return bytes_written;
+
 }
 
 
@@ -68,3 +60,18 @@ ssize_t producer(const char *user_buffer, size_t count){
  * In simplest terms, you would read the data from 
  * the circular buffer to ensure that there is no
  * overhead data available. */
+
+static int __init producer_consumer_init(void){
+	spin_lock_init(&lock_buffer);
+	init_waitqueue_head(&wait_queue);
+	printk(KERN INFO "Producer-Consumer Problem Code is now enterring successfully");
+	return 0;
+}
+
+static void __exit producer_consumer_init(void){
+	printk(KERN_INFO "Producer-Consumer Problem Code is now successfully exiting");
+
+}
+module_init(producer_consumer_init);
+module_exit(producer_consumer_exit);
+MODULE_LICENSE("GPL");
